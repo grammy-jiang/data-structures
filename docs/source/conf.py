a866c8299c0af2d3af185b1515bd45fb2d350264
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-16 21:42:28 +1000

    Enable extensions for sphinx
    This commit enables the following extensions for sphinx, and adds
    related settings of them:
    
     * sphinx.ext.autodoc
     * sphinx.ext.coverage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,16 +10,17 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path("/").joinpath(*Path(__file__).parts[:-3])))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Python Data Structures'
-copyright = '2019, Grammy Jiang'
-author = 'Grammy Jiang'
+project = "Python Data Structures"
+copyright = "2019, Grammy Jiang"
+author = "Grammy Jiang"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,11 +28,12 @@ author = 'Grammy Jiang'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage"]
+
+autoclass_content = 'both'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,9 +46,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
