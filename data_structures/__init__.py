164bed35535c58d316e89d2c989d17bf81e4b054
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-20 23:40:06 +1000

    Add comments in data_structures.__init__

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -1,3 +1,10 @@
+"""
+Data structures implemented by Python
+
+ * Linked List
+   * Singly Linked List
+   * Doubly Linked List
+"""
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
